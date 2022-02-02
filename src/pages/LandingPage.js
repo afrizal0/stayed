acf1626dc044422adc230f1f28269f1e053cd5e0
MUH. AFRIZAL <87596110+afrizal0@users.noreply.github.com>
2022-02-02 17:35:14 +0800

    refactor code

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,7 +4,6 @@ import Header from "parts/Header";
 
 export default class LandingPage extends Component {
   render() {
-    console.log(this.props);
     return (
       <>
         <Header {...this.props}></Header>
